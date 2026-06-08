6b1e4087952179961d85fc0fbc1fbc643bf7a407
wanessasalesa-jpg <wanessa.salesa@gmail.com>
2026-06-08 11:38:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
 st.title("Teste de Conexão")
 
 try:
-    conn = st.connection("gsheets", type="gsheets")
+    conn = st.connection("gsheets", type=GSheetsConnection)
     df = conn.read(worksheet="Escalacao", ttl=0)
-    st.success("Conexão feita! A planilha foi lida.")
+    st.success("Conexão feita! A planilha foi lida com sucesso.")
     st.write(df.head())
 except Exception as e:
-    st.error(f"Erro detectado: {e}")
+    st.error(f"Erro ao conectar: {e}")
