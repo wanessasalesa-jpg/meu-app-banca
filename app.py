7207e0437bb593a45372420e05098c2ed654136c
wanessasalesa-jpg <wanessa.salesa@gmail.com>
2026-06-08 15:13:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
 import streamlit as st
-import pandas as pd
-from streamlit_gsheets import GSheetsConnection
-
-st.title("Sistema Crivo")
-conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Escalacao", ttl=0)
-st.write(df.head())
+st.title("Teste Simples")
+st.write("Se isso aparecer, o sistema funciona.")
