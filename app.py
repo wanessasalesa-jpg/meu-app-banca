357b386ccaf1cc781760964ae6f6b3abe7fcfc8c
wanessasalesa-jpg <wanessa.salesa@gmail.com>
2026-06-08 11:43:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-st.title("Teste de Conexão")
+st.title("Sistema de Bancas")
 
-try:
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    df = conn.read(worksheet="Escalacao", ttl=0)
-    st.success("Conexão feita! A planilha foi lida com sucesso.")
-    st.write(df.head())
-except Exception as e:
-    st.error(f"Erro ao conectar: {e}")
+# Conexão Simples
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read(worksheet="Escalacao", ttl=0)
+
+st.success("Conectado com sucesso!")
+st.write(df.head())
